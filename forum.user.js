a923c44e63ec807248fe8266640a2e112d18ed0c
Hampton <hampmoe@icloud.com>
2017-07-06 14:47:49 -0400

    Update forum.user.js

--- forum.user.js
+++ forum.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Forum
 // @namespace    https://github.com/ScratchAT/CustomForum/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       herohamp
 // @match        https://scratch.mit.edu/*
