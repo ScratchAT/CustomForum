452f58bd09a80c74721bb6cd7e64fb4c5798ff57
Hampton <hampmoe@icloud.com>
2017-07-06 15:20:46 -0400

    Update forum.user.js

--- forum.user.js
+++ forum.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Forum
+// @name         ForumTesting
 // @namespace    https://github.com/ScratchAT/CustomForum/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @updateURL    https://github.com/ScratchAT/CustomForum/raw/master/forum.user.js
 // @author       herohamp
@@ -20,5 +20,27 @@
         $("tbody")[0].innerHTML="";
         var replaced = $("body").html().replace(/Advanced Topics/g,'The Secret Forum');
         $("body").html(replaced);
+        
+        function add(info){
+            $("tbody").append(`<tr><td class="tcl"><div class="intd"><div class="nosize"></div></div><div class="inew"><div class="nosize"></div></div><div class="tclcon"><h3><a href="${info.links}">${info.postName}</a></h3><span class="byuser"> by ${info.owner}</span></div></td><td class="tc2">${info.reps}</td><td class="tc3">${info.views}</td><td class="tcr"><a href="${info.links}">${info.latestdate}</a> <span class="byuser">by ${post.latestowner}</span></td></tr>`);
+
+        }
+        
+        
+        //just for testing
+        var post = {
+            link:"/discuss/-1/1",
+            postName:"Welcome",
+            reps:"1",
+            views:"99999",
+            owner:"DatOneLefty",
+            date:"1499353235",
+            latestowner:"herohamp",
+            latestdate:"1499353236"
+
+        }
+        
+        add(post);
+        
     }
 })();
