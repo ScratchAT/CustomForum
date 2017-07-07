981ddcc6276f1168a4abaf888abf7c48c6d7fb2d
herohamp <hampmoe@me.com>
2017-07-07 01:00:49 +0000

    Remote Loading

--- server/app.js
+++ server/app.js
@@ -1,1 +1,43 @@
-//server is here
+const express = require('express')
+const app = express()
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+//temp
+const posts = [{
+            link:"/discuss/-1/1",
+            postName:"Welcome",
+            reps:"1",
+            views:"99999",
+            owner:"DatOneLefty",
+            date:"1499353235",
+            latestowner:"herohamp",
+            latestdate:"149935323543"
+
+        },{
+            link:"/discuss/-1/1",
+            postName:"Welcome2",
+            reps:"1",
+            views:"99999",
+            owner:"DatOneLefty",
+            date:"1499353235",
+            latestowner:"herohamp",
+            latestdate:"14993532352"
+
+        }]
+
+app.get('/', function (req, res) {
+  res.redirect('https://scratch.mit.edu/discuss/-1/');
+})
+
+app.get('/posts', function (req, res) {
+  res.send(JSON.stringify(posts));
+})
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 3000!')
+})
