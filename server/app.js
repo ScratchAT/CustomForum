f130dc045a23f67e4418a76919956286d054e14c
Benny <blaze349@protonmail.com>
2017-07-07 01:59:53 +0000

    Add abiliy to send posts

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -8,7 +11,7 @@ app.use(function(req, res, next) {
 });
 
 //temp
-const posts = [{
+const topics = [{
             link:"/discuss/-1/1",
             postName:"Welcome",
             reps:"1",
@@ -16,7 +19,8 @@ const posts = [{
             owner:"DatOneLefty",
             date:"1499353235",
             latestowner:"herohamp",
-            latestdate:"149935323543"
+            latestdate:"149935323543",
+            posts: []
 
         },{
             link:"/discuss/-1/1",
@@ -26,7 +30,8 @@ const posts = [{
             owner:"DatOneLefty",
             date:"1499353235",
             latestowner:"herohamp",
-            latestdate:"14993532352"
+            latestdate:"14993532352",
+            posts: []
 
         }]
 
@@ -34,9 +39,14 @@ app.get('/', function (req, res) {
   res.redirect('https://scratch.mit.edu/discuss/-1/');
 })
 
-app.get('/posts', function (req, res) {
-  res.send(JSON.stringify(posts));
+app.get('/topics', function (req, res) {
+  res.send(JSON.stringify(topics));
 })
+
+app.post('/:topic') {
+  topics[req.params.topic].posts += req.body.post;
+  res.send("Post send")
+}
 
 app.listen(8080, function () {
   console.log('Example app listening on port 3000!')
